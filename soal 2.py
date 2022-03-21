45b96d02200bee617c4cddd3d3488988c23a2eb7
Esa Putra <esaraharjo72@gmail.com>
2022-03-21 13:07:47 +0700

    tugas 1

--- soal 2.py
+++ soal 2.py
@@ -1,5 +1,6 @@
 # input 
-pi = 22/7
+pi= 22/7
+print("\u03C0 = 22/7") 
 r1 = 15
 r2 = 34
 
@@ -8,5 +9,13 @@ l1 = pi * r1**2
 l2 = pi * r2**2
 
 # jawaban 
-txt1 = "luas lingkaran dengan jari -jari {} cm, adalah {} cm print("\u00b2")". format (r1, l1)
+txt1 = "luas lingkaran dengan jari -jari {} cm, adalah {} cm\u00b2". format (r1,l1)
+txt2 = "luas lingkaran dengan jari -jari {} cm, adalah {} cm\u00b2". format (r2, l2)
 print(txt1)
+print(txt2)
+
+# bonus
+txt3 = "luas lingkaran dengan jari -jari {} cm, adalah {:.2f} cm\u00b2". format (r1,l1)
+txt4 = "luas lingkaran dengan jari -jari {} cm, adalah {:.2f} cm\u00b2". format (r2,l2)
+print(txt3)
+print(txt4)
