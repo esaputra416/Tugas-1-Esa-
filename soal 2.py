57037d6769761bb3b6ef03037fa13de1d5a3e774
Esa Putra <esaraharjo72@gmail.com>
2022-03-21 12:13:22 +0700

    tugas 1

--- soal 2.py
+++ soal 2.py
@@ -1,1 +1,12 @@
 # input 
+pi = 22/7
+r1 = 15
+r2 = 34
+
+# kalkulasi 
+l1 = pi * r1**2
+l2 = pi * r2**2
+
+# jawaban 
+txt1 = "luas lingkaran dengan jari -jari {} cm, adalah {} cm print("\u00b2")". format (r1, l1)
+print(txt1)
